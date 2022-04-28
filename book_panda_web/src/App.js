863d77ea632391018b1972ff1104e2ba88738bf6
marianlupuleasa <72737446+marianlupuleasa@users.noreply.github.com>
2022-04-28 19:54:25 +0300

    added web

---
+++ book_panda_web/src/App.js
@@ -0,0 +1,33 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/AuthContext";
+
+function App() {
+  const authContext = useContext(AuthContext);
+  const isLoggedin = authContext.isLoggedin;
+
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/auth">{!isLoggedin && <AuthPage />}</Route>
+        <Route path="/profile">
+          {isLoggedin && <UserProfile />}
+          {!isLoggedin && <Redirect to="/auth" />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
